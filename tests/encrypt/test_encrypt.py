136b80e3831f0b6ce053c11b7da756ea79b4bd95
DearJao <jvvr2013@gmail.com>
2023-03-22 18:38:56 -0300

    algorithm to verify palindrome words

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,17 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    # Test with key as non-positive integer
+    with pytest.raises(ValueError):
+        encrypt_message(-1, "hello")
+
+    # Test with key greater than message length
+    assert encrypt_message("buapau", 9) == "uapaub"
+
+    # Test with odd key
+    assert encrypt_message("buapau", 3) == "aub_uap"
+
+    # Test with even key
+    assert encrypt_message("buapau", 4) == "ua_paub"
