a76ffe84f848a1a4e162f8601432dfb4a6054564
DearJao <jvvr2013@gmail.com>
2023-03-23 16:50:09 -0300

    fix

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,15 +3,11 @@ import pytest
 
 
 def test_encrypt_message():
-    # Test with key as non-positive integer
-    with pytest.raises(ValueError):
-        encrypt_message(-1, "hello")
-
-    # Test with key greater than message length
     assert encrypt_message("buapau", 9) == "uapaub"
 
-    # Test with odd key
     assert encrypt_message("buapau", 3) == "aub_uap"
 
-    # Test with even key
     assert encrypt_message("buapau", 4) == "ua_paub"
+
+    with pytest.raises(TypeError):
+        encrypt_message("hello", "1")
